21d202bcc15b54e8b33877535343d64ae9eeeb41
Dilan <dilan.hansike@gmail.com>
2025-11-22 19:37:20 -0500

    init push

--- src/main/java/com/aurelius/tech/eventmanagementservice/dto/response/AuthResponse.java
+++ src/main/java/com/aurelius/tech/eventmanagementservice/dto/response/AuthResponse.java
@@ -1,1 +1,21 @@
+package com.aurelius.tech.eventmanagementservice.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    
+    private String accessToken;
+    private String refreshToken;
+    private UUID id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String role;
+}
